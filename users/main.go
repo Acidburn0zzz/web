1fb7153cf9545efa9d905f99322ff0f2fc01b8d1
Dylan Johnston <qdylanj@gmail.com>
2015-10-31 08:04:09 +0100

    use api helper

--- users/main.go
+++ users/main.go
@@ -8,18 +8,7 @@ import (
 	"io"
 )
 
-func usersIndex(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Welcome to the Dashboard")
-}
-
 func main() {
-	database := "zqz-users-dev"
-	dbuser := "dylan"
-	port := 9000
-
-	s := app.NewApp(port)
-	s.AddDatabase(database, dbuser)
-	s.AddRoute("GET", "/", usersIndex)
-	s.AddRoute("POST", "/", api.UserCreate)
-	s.Listen()
+	api := NewUserAPI("zqz-users-dev", "dylan", 5000)
+	api.App.Listen()
 }
